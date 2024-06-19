92410f1da79798438a8e24341d2ef57f19189bcf
Justin B <46439389+JustinBrow@users.noreply.github.com>
2024-06-19 11:34:16 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function main() {
       localStorage.getItem("adsEnabled") && (this.settingsData.adsEnabled = localStorage.getItem("adsEnabled"),
       localStorage.getItem("adsAnalytics") && (this.settingsData.adsEnabled = localStorage.getItem("adsAnalytics")
    } catch {
-      alert("error in main");
+      alert("error in main")
    }
 }
 
